d86a3be1caf9cc7d30e1eac529e1175108a08489
Latrinum <sams2631@gmail.com>
2015-12-20 18:46:42 +0000

    Finished Day 17.

---
+++ AdventofCode2015/Day 17/day17.py
@@ -0,0 +1,51 @@
+
+inputFile = open('input.txt', 'r')
+
+totalStore = 150
+
+containers = list()
+minNum = 1000
+containerTrack = list()
+
+def getPerms(sizeLeft, index, num, key) :
+    perms = 0
+    global minNum
+
+    if key == 'part2' and num > minNum :
+        return 0
+    if sizeLeft == 0 :
+        if num < minNum :
+            minNum = num
+        return 1
+    elif sizeLeft <= 0 :
+        return 0
+    
+    for j in range(index + 1, len(containers)) :
+        perms += getPerms(sizeLeft - containers[j], j, num + 1, key)
+            
+    return perms
+
+def fillContainers() :
+
+    for line in inputFile :
+        containers.append(int(line))
+
+    containers.sort()
+    containers.reverse()
+
+fillContainers()
+print(containers)
+
+perms = 0
+
+for i in range(0, len(containers)) :
+    perms += getPerms(totalStore - containers[i], i, 1, 'part1')
+
+print("part1 = {}".format(perms))
+
+perms = 0
+
+for i in range(0, len(containers)) :
+    perms += getPerms(totalStore - containers[i], i, 1, 'part2')
+    
+print("part2 = {}".format(perms))
